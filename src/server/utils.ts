231680da582de33e04e692c54adbeeb42af0310a
Ying Xue <yingxue@microsoft.com>
2025-01-16 13:29:22 -0800

    fix codeql spotted issues

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -19,9 +19,14 @@ export const fetchDirectories = (dir: string, res: express.Response) => {
     const result: string[] = [];
     for (const item of fs.readdirSync(dir)) {
         try {
-            const stat = fs.statSync(path.join(dir, item));
-            if (stat.isDirectory()) {
-                result.push(item);
+            const root = path.resolve(dir);
+            const filePath =fs.realpathSync(path.resolve(dir, item));
+
+            if (filePath.startsWith(root)) {
+                const stat = fs.statSync(filePath);
+                if (stat.isDirectory()) {
+                    result.push(item);
+                }
             }
         }
         catch {
